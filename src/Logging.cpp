69b63459535051b5176e59f0e53083975fe5c1e7
cnweaver <cnweaver@gmail.com>
2018-06-25 15:56:13 -0500

    Remove unused includes

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,6 +1,4 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/date_time/local_time_adjustor.hpp>
-#include <boost/date_time/c_local_time_adjustor.hpp>
 
 std::string timestamp(){
 	auto now = boost::posix_time::second_clock::universal_time();
