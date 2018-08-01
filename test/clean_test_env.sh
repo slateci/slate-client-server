0305901fc4504e0c356ad5349480263e755c3e81
cnweaver <cnweaver@gmail.com>
2018-08-01 18:51:28 -0500

    If the test minikube instance is already running, use it without starting or stopping

--- test/clean_test_env.sh
+++ test/clean_test_env.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-echo "Stopping minikube"
-minikube -p 'slate_server_test_kube' stop
+if [ -f .stop_minikube_after_tests ]; then
+	echo "Stopping minikube"
+	minikube -p 'slate_server_test_kube' stop
+	rm .stop_minikube_after_tests
+fi
 echo "Stopping Dynamo server"
 curl -s -X PUT 'http://localhost:52000/stop'
--- test/init_test_env.sh
+++ test/init_test_env.sh
@@ -6,22 +6,35 @@ INITIAL_DIR=`pwd`
 wait_pod_ready(){
 	PODNAME=$1;
 	echo "Waiting for $PODNAME to be ready"
-	REPLICAS="0 -eq 1"
-	until [ $REPLICAS ]; do
-	sleep 1
+	READY="0 -eq 1"
+	unset DELAY # don't sleep on first iteration
+	until [ $READY ]; do
+		if [ "$DELAY" ]; then
+			sleep $DELAY
+		else
+			DELAY=1
+		fi
 		STATUS=`kubectl get pods --namespace kube-system 2>/dev/null | grep "$PODNAME"`
-		REPLICAS=`echo "$STATUS" | awk '{print $2}'`
-		echo "    Replicas: $REPLICAS ("`echo "$STATUS" | awk '{print $3}'`")"
-		REPLICAS=`echo "$REPLICAS" | sed 's|\(.*\)/\(.*\)|\1 -eq \2|'`
+		COUNT=`echo "$STATUS" | awk '{print $2}'`
+		echo "    Containers: $COUNT ("`echo "$STATUS" | awk '{print $3}'`")"
+		READY=`echo "$COUNT" | sed 's|\(.*\)/\(.*\)|\1 -eq \2|'`' -a '`echo "$STATUS" | awk '{print $3}'`' = Running'
 	done
 }
 
-echo "Starting minikube"
-minikube -p 'slate_server_test_kube' start
+MINIKUBE_STATUS=`minikube -p 'slate_server_test_kube' status`
+if [ `echo "$MINIKUBE_STATUS" | grep -c Running` -eq 2 \
+    -a `echo "$MINIKUBE_STATUS" | grep -c 'Correctly Configured'` -eq 1 ] ; then
+	echo "Using running minikube instance"
+else
+	echo "Starting minikube"
+	touch .stop_minikube_after_tests
+	minikube -p 'slate_server_test_kube' start
+fi
 wait_pod_ready "kube-apiserver-minikube"
 # these components get nasty auto-generated names, but have predictable prefixes
 wait_pod_ready "kube-proxy"
 wait_pod_ready "kube-dns"
+
 echo "Starting Dynamo server"
 ./slate-test-database-server &
 sleep 1 # wait for server to start
