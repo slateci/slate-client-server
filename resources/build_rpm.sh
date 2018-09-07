e6703c5e2515b7fd338b69f6780377a1b430315a
cnweaver <cnweaver@gmail.com>
2018-09-07 17:37:39 -0500

    Quote everything in case of spaces in paths

--- resources/build_rpm.sh
+++ resources/build_rpm.sh
@@ -4,29 +4,29 @@ set -e
 
 PKG_NAME="slate-api-server-0.1.0"
 PKG_SHORT_NAME="slate-api-server"
-mkdir -p ${CMAKE_BINARY_DIR}/SOURCES
+mkdir -p "${CMAKE_BINARY_DIR}/SOURCES"
 
-rm -rf ${CMAKE_BINARY_DIR}/SOURCES/${PKG_SHORT_NAME} ${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz
+rm -rf "${CMAKE_BINARY_DIR}/SOURCES/${PKG_SHORT_NAME}" "${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz"
 
 # Get a clean copy of the source tree into a tarball
-if which svn > /dev/null 2>&1 && svn info ${CMAKE_SOURCE_DIR} > /dev/null 2>&1 ; then
-	svn export -q ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR}/SOURCES/${PKG_SHORT_NAME}
-	tar czf ${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz --exclude "${PKG_SHORT_NAME}/\.*" \
-	    -C ${CMAKE_BINARY_DIR}/SOURCES ${PKG_SHORT_NAME}
-elif which git > /dev/null 2>&1 && [ -d ${CMAKE_SOURCE_DIR}/.git ] ; then
-	SRC_DIR_NAME=$(basename ${CMAKE_SOURCE_DIR})
-	git clone ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR}/SOURCES/${PKG_SHORT_NAME}
-	tar czf ${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz -C ${CMAKE_BINARY_DIR}/SOURCES \
-	    --exclude "${PKG_SHORT_NAME}/\.*" ${PKG_SHORT_NAME}
+if which svn > /dev/null 2>&1 && svn info "${CMAKE_SOURCE_DIR}" > /dev/null 2>&1 ; then
+	svn export -q "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}/SOURCES/${PKG_SHORT_NAME}"
+	tar czf "${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz" --exclude "${PKG_SHORT_NAME}/\.*" \
+	    -C "${CMAKE_BINARY_DIR}/SOURCES" "${PKG_SHORT_NAME}"
+elif which git > /dev/null 2>&1 && [ -d "${CMAKE_SOURCE_DIR}/.git" ] ; then
+	SRC_DIR_NAME=$(basename "${CMAKE_SOURCE_DIR}")
+	git clone "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}/SOURCES/${PKG_SHORT_NAME}"
+	tar czf "${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz" -C "${CMAKE_BINARY_DIR}/SOURCES" \
+	    --exclude "${PKG_SHORT_NAME}/\.*" "${PKG_SHORT_NAME}"
 else
 	# git lacks a proper export command, so treat it as unversion controlled and do everything manually
-	SRC_DIR_NAME=$(basename ${CMAKE_SOURCE_DIR})
+	SRC_DIR_NAME=$(basename "${CMAKE_SOURCE_DIR}")
 	# $CMAKE_BINARY_DIR might be inside $CMAKE_SOURCE_DIR, so we need to exclude it from the tarball
 	# To do that, we need to figure out its name realtive to $CMAKE_SOURCE_DIR. We can do that by
 	# checking whether $CMAKE_SOURCE_DIR is a prefix of $CMAKE_BINARY_DIR, but we must take care to
 	# ensure that the prefix we use for checking ends with a slash to avoid being fooled by
 	# stuff/package being a prefix of stuff/package_build or similar. 
-	DIR_PREFIX=${CMAKE_SOURCE_DIR}
+	DIR_PREFIX="${CMAKE_SOURCE_DIR}"
 	if echo "${DIR_PREFIX}" | grep -q "[^/]$"; then
 		# append a slash if one was not already there
 		DIR_PREFIX="${DIR_PREFIX}/"
@@ -43,10 +43,10 @@ else
 	echo "\.DS_Store" >> export_exclusions
 	# Note: running this on darwin leaves lots of ._ clutter, but this shouldn't matter as rpmbuild 
 	#       should only be relevant on linux. Emacs/vim clutter might be a problem, however. 
-	tar czf ${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz -C ${CMAKE_SOURCE_DIR}/.. \
-	    -X export_exclusions ${SRC_DIR_NAME}
+	tar czf "${CMAKE_BINARY_DIR}/SOURCES/${PKG_NAME}.tar.gz" -C "${CMAKE_SOURCE_DIR}/.." \
+	    -X export_exclusions "${SRC_DIR_NAME}"
 	rm export_exclusions
 fi
 
 # Exceuting rpmbuild
-rpmbuild --define "_topdir ${CMAKE_BINARY_DIR}" -ba ${CMAKE_SOURCE_DIR}/resources/rpm_specs/slate-api-server.spec
+rpmbuild --define "_topdir ${CMAKE_BINARY_DIR}" -ba "${CMAKE_SOURCE_DIR}/resources/rpm_specs/slate-api-server.spec"
