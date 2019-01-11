61c7a8f17d1e01b160610ce42ab7a29b205de142
cnweaver <cnweaver@gmail.com>
2019-01-11 17:28:15 -0600

    No need to mass delete clusters during cleanup anymore

--- test/clean_test_env.sh
+++ test/clean_test_env.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-# Make sure we are in the right place before doing a mass delete
-kubectl config use-context 'slate-server-test-kube'
-if [ "$?" -eq 0 ]; then # only if setting context succeeded
-	echo "Deleting all 'clusters'"
-	CLUSTERS=$(kubectl get clusters -o jsonpath='{.items[*].metadata.name}')
-	if [ "$CLUSTERS" ]; then
-		kubectl delete clusters $CLUSTERS > /dev/null
-	fi
-	unset CLUSTERS
-fi
 if [ -f .stop_minikube_after_tests ]; then
 	echo "Stopping minikube"
 	minikube -p 'slate-server-test-kube' stop
