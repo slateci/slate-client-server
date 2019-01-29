d398c096619784e66fb9527a0392fc442aafbe65
cnweaver <cnweaver@gmail.com>
2019-01-29 11:16:13 -0600

    Extract meaningful version numbers from git

--- cmake/embed_version.sh
+++ cmake/embed_version.sh
@@ -18,10 +18,14 @@ fi
 if [ -z ${DONE} ]; then
 	cd "${SRC_DIR}" # git is retarded and cannot be pointed to other directories
 	if which git > /dev/null 2>&1; then
-		VERSION=$(git rev-parse HEAD)
+		# attempt to simulate sane version numbering
+		VERSION=$(git log | grep -c '^commit')
 		if [ -z "$VERSION" ]; then
 			VERSION="unknown version"
 		else
+			if git status -s -uno | grep -q '^.M'; then
+				VERSION=${VERSION}M
+			fi
 			DONE=1
 		fi
 	fi
