1473ea5108ceab66d398a51ad6b495bec017f429
cnweaver <cnweaver@gmail.com>
2019-02-21 14:50:34 -0600

    Add tests for restarting instances

---
+++ test/TestInstanceRestarting.cpp
@@ -0,0 +1,174 @@
+#include "test.h"
+
+#include <ServerUtilities.h>
+
+TEST(UnauthenticatedRestartInstance){
+	using namespace httpRequests;
+	TestContext tc;
+	
+	//try deleting a VO with no authentication
+	auto resResp=httpPut(tc.getAPIServerURL()+"/"+currentAPIVersion+"/instances/Instance_ABCD/restart","");
+	ENSURE_EQUAL(resResp.status,403,
+				 "Requests to restart instances without authentication should be rejected");
+	
+	//try deleting a VO with invalid authentication
+	resResp=httpPut(tc.getAPIServerURL()+"/"+currentAPIVersion+"/instances/Instance_ABCD/restart?token=00112233-4455-6677-8899-aabbccddeeff","");
+	ENSURE_EQUAL(resResp.status,403,
+				 "Requests to restart instances with invalid authentication should be rejected");
+}
+
+TEST(RestartInstance){
+	using namespace httpRequests;
+	TestContext tc;
+	
+	std::string adminKey=getPortalToken();
+	auto schema=loadSchema(getSchemaDir()+"/InstanceListResultSchema.json");
+	
+	std::string voName="test-inst-restart";
+	std::string clusterName="testcluster";
+	
+	{ //create a VO
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", voName, alloc);
+		metadata.AddMember("scienceField", "Logic", alloc);
+		request.AddMember("metadata", metadata, alloc);
+		auto createResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/vos?token="+adminKey,to_string(request));
+		ENSURE_EQUAL(createResp.status,200,"VO creation request should succeed");
+	}
+	
+	{ //create a cluster
+		auto kubeConfig = tc.getKubeConfig();
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", clusterName, alloc);
+		metadata.AddMember("vo", voName, alloc);
+		metadata.AddMember("organization", "Department of Labor", alloc);
+		metadata.AddMember("kubeconfig", kubeConfig, alloc);
+		request.AddMember("metadata", metadata, alloc);
+		auto createResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/clusters?token="+adminKey, to_string(request));
+		ENSURE_EQUAL(createResp.status,200,
+					 "Cluster creation request should succeed");
+		ENSURE(!createResp.body.empty());
+	}
+	
+	std::string instID;
+	{ //install an instance
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		request.AddMember("vo", voName, alloc);
+		request.AddMember("cluster", clusterName, alloc);
+		request.AddMember("tag", "install1", alloc);
+		request.AddMember("configuration", "", alloc);
+		auto instResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/apps/test-app?test&token="+adminKey,to_string(request));
+		ENSURE_EQUAL(instResp.status,200,"Application install request should succeed");
+		rapidjson::Document data;
+		data.Parse(instResp.body);
+		if(data.HasMember("metadata") && data["metadata"].IsObject() && data["metadata"].HasMember("id"))
+			instID=data["metadata"]["id"].GetString();
+	}
+	
+	{ //restart the instance
+		auto resResp=httpPut(tc.getAPIServerURL()+"/"+currentAPIVersion+"/instances/"+instID+"/restart?token="+adminKey,"");
+		ENSURE_EQUAL(resResp.status,200,"Instance restart request should succeed");
+	}
+}
+
+TEST(UnrelatedUserDeleteInstance){
+	using namespace httpRequests;
+	TestContext tc;
+	
+	std::string adminKey=getPortalToken();
+	auto schema=loadSchema(getSchemaDir()+"/InstanceListResultSchema.json");
+	
+	std::string voName="test-inst-restart-unrelus";
+	std::string clusterName="testcluster";
+	
+	{ //create a VO
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", voName, alloc);
+		metadata.AddMember("scienceField", "Logic", alloc);
+		request.AddMember("metadata", metadata, alloc);
+		auto createResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/vos?token="+adminKey,to_string(request));
+		ENSURE_EQUAL(createResp.status,200,"VO creation request should succeed");
+	}
+	
+	{ //create a cluster
+		auto kubeConfig = tc.getKubeConfig();
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", clusterName, alloc);
+		metadata.AddMember("vo", voName, alloc);
+		metadata.AddMember("organization", "Department of Labor", alloc);
+		metadata.AddMember("kubeconfig", kubeConfig, alloc);
+		request.AddMember("metadata", metadata, alloc);
+		auto createResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/clusters?token="+adminKey, to_string(request));
+		ENSURE_EQUAL(createResp.status,200,
+					 "Cluster creation request should succeed");
+		ENSURE(!createResp.body.empty());
+	}
+	
+	std::string instID;
+	struct cleanupHelper{
+		TestContext& tc;
+		const std::string& id, key;
+		cleanupHelper(TestContext& tc, const std::string& id, const std::string& key):
+		tc(tc),id(id),key(key){}
+		~cleanupHelper(){
+			if(!id.empty())
+				auto delResp=httpDelete(tc.getAPIServerURL()+"/"+currentAPIVersion+"/instances/"+id+"?token="+key);
+		}
+	} cleanup(tc,instID,adminKey);
+	
+	{ //install an instance
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		request.AddMember("vo", voName, alloc);
+		request.AddMember("cluster", clusterName, alloc);
+		request.AddMember("tag", "install1", alloc);
+		request.AddMember("configuration", "", alloc);
+		auto instResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/apps/test-app?test&token="+adminKey,to_string(request));
+		ENSURE_EQUAL(instResp.status,200,"Application install request should succeed");
+		rapidjson::Document data;
+		data.Parse(instResp.body);
+		if(data.HasMember("metadata") && data["metadata"].IsObject() && data["metadata"].HasMember("id"))
+			instID=data["metadata"]["id"].GetString();
+	}
+	
+	std::string tok;
+	{ //create an unrelated user
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", "Bob", alloc);
+		metadata.AddMember("email", "bob@place.com", alloc);
+		metadata.AddMember("phone", "555-5555", alloc);
+		metadata.AddMember("institution", "Center of the Earth University", alloc);
+		metadata.AddMember("admin", false, alloc);
+		metadata.AddMember("globusID", "Bob's Globus ID", alloc);
+		request.AddMember("metadata", metadata, alloc);
+		auto createResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/users?token="+adminKey,to_string(request));
+		ENSURE_EQUAL(createResp.status,200,"User creation request should succeed");
+		rapidjson::Document createData;
+		createData.Parse(createResp.body);
+		tok=createData["metadata"]["access_token"].GetString();
+	}
+	
+	{ //restart the instance
+		auto resResp=httpPut(tc.getAPIServerURL()+"/"+currentAPIVersion+"/instances/"+instID+"/restart?token="+tok,"");
+		ENSURE_EQUAL(resResp.status,403,
+		            "Requests to restart application instances from unrelated users should be rejected");
+	}
+}
