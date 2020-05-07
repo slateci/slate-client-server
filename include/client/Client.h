32eecd5c2819d42e80cec491cda356366c5b4382
Joshua Cragun <jmcragun@gmail.com>
2020-05-07 17:57:07 -0600

    removing levensteinDistance from Client.h

--- include/client/Client.h
+++ include/client/Client.h
@@ -369,39 +369,6 @@ private:
 	
 	void ensureRBAC(const std::string& configPath, bool assumeYes);
 	bool checkLoadBalancer(const std::string& configPath, bool assumeYes);
-	
-	// Code is available under the Creative Commons Attribution-ShareAlike License
-	// Source: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C++
-	template<typename T>
-	typename T::size_type levensteinDistance(const T &source, const T &target){
-    	if (source.size() > target.size()) {
-        	return levensteinDistance(target, source);
-    	}
-
-    	using TSizeType = typename T::size_type;
-    	const TSizeType min_size = source.size(), max_size = target.size();
-    	std::vector<TSizeType> lev_dist(min_size + 1);
-
-    	for (TSizeType i = 0; i <= min_size; ++i) {
-        	lev_dist[i] = i;
-    	}
-
-    	for (TSizeType j = 1; j <= max_size; ++j) {
-        	TSizeType previous_diagonal = lev_dist[0], previous_diagonal_save;
-        	++lev_dist[0];
-
-        	for (TSizeType i = 1; i <= min_size; ++i) {
-            	previous_diagonal_save = lev_dist[i];
-            	if (source[i - 1] == target[j - 1]) {
-                		lev_dist[i] = previous_diagonal;
-            	} else {
-                	lev_dist[i] = std::min(std::min(lev_dist[i - 1], lev_dist[i]), previous_diagonal) + 1;
-            	}
-            	previous_diagonal = previous_diagonal_save;
-        	}
-    	}
-    	return lev_dist[min_size];
-	};
 
 	template<typename OptionsType>
     void retryInstanceCommandWithFixup(void (Client::* command)(const OptionsType&), OptionsType opt);
