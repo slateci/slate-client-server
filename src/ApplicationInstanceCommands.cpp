870c5cc35447a4ac76889f5ad9d1347ab53f3aa6
Lincoln Bryant <lincolnb@uchicago.edu>
2020-06-19 12:41:08 -0500

    first pass at a patch for reporting the external address

--- src/ApplicationInstanceCommands.cpp
+++ src/ApplicationInstanceCommands.cpp
@@ -163,7 +163,42 @@ std::multimap<std::string,ServiceInterface> getServices(const SharedFileHandle& 
 				continue;
 			}
 			if(podData["items"][0]["status"].HasMember("hostIP"))
-				interface.externalIP=podData["items"][0]["status"]["hostIP"].GetString();
+				// now we should get the node info, to see if it has a ExternalIP which we ought to preferentially use
+				if(podData["items"][0]["spec"].HasMember("nodeName")) {
+					auto nodename=podData["Items"][0]["spec"]["nodeName"]
+
+					t1 = high_resolution_clock::now();
+					auto nodeResult=kubernetes::kubectl(*configPath,{"get","node",nodename,"-o=json"});
+					t2 = high_resolution_clock::now();
+					log_info("kubectl get node completed in " << duration_cast<duration<double>>(t2-t1).count() << " seconds");
+					if(nodeResult.status){
+						log_error("kubectl get node " << node << nspace << " failed: " << nodeResult.error);
+						continue;
+					}
+
+					rapidjson::Document nodeData;
+					try{
+						nodeData.Parse(nodeResult.output.c_str());
+					}catch(std::runtime_error& err){
+						log_error("Unable to parse kubectl node JSON output for kubectl get node " << nodename << ": " << err.what());
+						continue;
+					}
+
+					if(nodeData.HasMember("status"){
+						for (auto& addr : nodeData["status"]["addresses"].GetArray()) {
+							std::string addrType(addr["type"].GetString());
+							// assume that externalIP won't show up more than once
+							if (addrType == "ExternalIP") {
+								interface.externalIP=addr["address"].GetString();
+							}else {
+								// fall back to the original way of setting the external IP via host IP reported in podData
+						   		interface.externalIP=podData["items"][0]["status"]["hostIP"].GetString();
+							}
+					}
+				} else {
+					// couldn't find a nodeName, fall back to host IP
+					interface.externalIP=podData["items"][0]["status"]["hostIP"].GetString();
+				}
 			else
 				interface.externalIP="<none>";
 		}
