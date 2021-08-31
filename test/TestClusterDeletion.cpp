a6462eb77be724f2343fb60967c67b77aa4f9181
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-08-31 10:22:42 -0600

    creating test to skipCascadingDeletion

--- test/TestClusterDeletion.cpp
+++ test/TestClusterDeletion.cpp
@@ -249,7 +249,7 @@ TEST(DeletingClusterHasCascadingDeletion){
 				auto delResp=httpDelete(tc.getAPIServerURL()+"/"+currentAPIVersion+"/secrets/"+id+"?token="+key);
 		}
 	} cleanup(tc,secretID,adminKey);
-	
+	 
 	std::string secretsURL=tc.getAPIServerURL()+"/"+currentAPIVersion+"/secrets?token="+adminKey;
 	{   // install a secret
 		rapidjson::Document request(rapidjson::kObjectType);
@@ -273,6 +273,155 @@ TEST(DeletingClusterHasCascadingDeletion){
 	}
 
 	// perform the deletion 
+	auto deleteResp=httpDelete(tc.getAPIServerURL()+"/"+currentAPIVersion+"/clusters/"+clusterID+
+				   "?token="+adminKey);
+	ENSURE_EQUAL(deleteResp.status,200,"Cluster deletion should succeed");
+	
+	// verify that everything else was deleted, too
+	DatabaseContext db;
+	auto storePtr=db.makePersistentStore();
+	auto& store=*storePtr;
+
+	auto instance = store.getApplicationInstance(instID);
+	auto secret = store.getSecret(secretID);
+	ENSURE_EQUAL(instance, ApplicationInstance(), "Cluster deletion should delete instances");
+	ENSURE_EQUAL(secret, Secret(), "Cluster deletion should delete secrets");
+
+	// Get kubeconfig, save it to file, and use it to check namespaces
+	std::string conf = tc.getKubeConfig();
+	std::ofstream out("testclusterconfigdeletion.yaml");
+	out << conf;
+	out.close();
+	std::vector<std::string> args = {"get", "namespaces"};
+	startReaper();
+	auto names = kubernetes::kubectl("./testconfigdeletion.yaml", args);
+	stopReaper();
+	ENSURE_EQUAL(names.output.find("slate-group-testgroup1"), std::string::npos, "Cluster deletion should delete associated namespaces");
+}
+
+TEST(ForceDeletingUnreachableCluster){
+	// Make a, VO, cluster, instance, and secrets
+	// Then verify the latter were deleted as a consequence of deleting the cluster
+	using namespace httpRequests;
+	TestContext tc;	
+	std::string adminKey=tc.getPortalToken();
+
+	// create Group to register cluster with
+	const std::string groupName="testgroup1";
+	rapidjson::Document createGroup(rapidjson::kObjectType);
+	{
+		auto& alloc = createGroup.GetAllocator();
+		createGroup.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", groupName, alloc);
+		metadata.AddMember("scienceField", "Logic", alloc);
+		createGroup.AddMember("metadata", metadata, alloc);
+	}
+	auto groupResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/groups?token="+adminKey,
+			     to_string(createGroup));
+	ENSURE_EQUAL(groupResp.status,200,"Group creation request should succeed");
+	rapidjson::Document groupData;
+	groupData.Parse(groupResp.body.c_str());
+	auto groupID=groupData["metadata"]["id"].GetString();	
+
+	auto kubeConfig = tc.getKubeConfig();
+
+	// create the cluster
+	const std::string clusterName="testcluster";
+	auto createClusterUrl=tc.getAPIServerURL()+"/"+currentAPIVersion+"/clusters?token="+adminKey;
+	rapidjson::Document request1(rapidjson::kObjectType);
+	{
+		auto& alloc = request1.GetAllocator();
+		request1.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", clusterName, alloc);
+		metadata.AddMember("group", rapidjson::StringRef(groupID), alloc);
+		metadata.AddMember("owningOrganization", "Department of Labor", alloc);
+		metadata.AddMember("kubeconfig", rapidjson::StringRef(kubeConfig), alloc);
+		request1.AddMember("metadata", metadata, alloc);
+	}
+	auto createResp=httpPost(createClusterUrl, to_string(request1));
+	ENSURE_EQUAL(createResp.status,200,
+		     "Cluster creation request should succeed");
+	rapidjson::Document createData;
+	createData.Parse(createResp.body);
+	auto clusterID=createData["metadata"]["id"].GetString();
+
+	std::string instID;
+	{ // install an instance
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		request.AddMember("group", groupName, alloc);
+		request.AddMember("cluster", clusterName, alloc);
+		request.AddMember("tag", "install1", alloc);
+		request.AddMember("configuration", "", alloc);
+		auto instResp=httpPost(tc.getAPIServerURL()+"/"+currentAPIVersion+"/apps/test-app?test&token="+adminKey,to_string(request));
+		ENSURE_EQUAL(instResp.status,200,"Application install request should succeed");
+		rapidjson::Document data;
+		data.Parse(instResp.body);
+		if(data.HasMember("metadata") && data["metadata"].IsObject() && data["metadata"].HasMember("id"))
+			instID=data["metadata"]["id"].GetString();
+	}
+
+	const std::string secretName="createsecret-secret1";
+	std::string secretID;
+	struct cleanupHelper{
+		TestContext& tc;
+		const std::string& id, key;
+		cleanupHelper(TestContext& tc, const std::string& id, const std::string& key):
+		tc(tc),id(id),key(key){}
+		~cleanupHelper(){
+			if(!id.empty())
+				auto delResp=httpDelete(tc.getAPIServerURL()+"/"+currentAPIVersion+"/secrets/"+id+"?token="+key);
+		}
+	} cleanup(tc,secretID,adminKey);
+	
+	std::string secretsURL=tc.getAPIServerURL()+"/"+currentAPIVersion+"/secrets?token="+adminKey;
+	{   // install a secret
+		rapidjson::Document request(rapidjson::kObjectType);
+		auto& alloc = request.GetAllocator();
+		request.AddMember("apiVersion", currentAPIVersion, alloc);
+		rapidjson::Value metadata(rapidjson::kObjectType);
+		metadata.AddMember("name", secretName, alloc);
+		metadata.AddMember("group", groupName, alloc);
+		metadata.AddMember("cluster", clusterName, alloc);
+		request.AddMember("metadata", metadata, alloc);
+		rapidjson::Value contents(rapidjson::kObjectType);
+		contents.AddMember("foo", encodeBase64("bar"), alloc);
+		request.AddMember("contents", contents, alloc);
+		auto createResp=httpPost(secretsURL, to_string(request));
+		ENSURE_EQUAL(createResp.status,200, "Secret creation should succeed: "+createResp.body);
+		rapidjson::Document data;
+		data.Parse(createResp.body.c_str());
+		auto schema=loadSchema(getSchemaDir()+"/SecretCreateResultSchema.json");
+		ENSURE_CONFORMS(data,schema);
+		secretID=data["metadata"]["id"].GetString();
+	}
+
+	// make cluster unreachable
+	//disable kubelet?
+	//set kubeconfig=blank?
+	auto kubeConfig = tc.getEmptyKubeConfig();
+
+	// delete cluster records and skip cascading deletion
+	auto deleteResp=httpDelete(tc.getAPIServerURL()+"/"+currentAPIVersion+"/clusters/"+clusterID+
+				   "?token="+adminKey);
+	ENSURE_EQUAL(deleteResp.status,200,"Cluster deletion should succeed");
+
+	// verify that database records were deleted
+	DatabaseContext db;
+	auto storePtr=db.makePersistentStore();
+	auto& store=*storePtr;
+
+	auto instance = store.getApplicationInstance(instID);
+	auto secret = store.getSecret(secretID);
+	ENSURE_EQUAL(instance, ApplicationInstance(), "Cluster deletion should delete instances");
+	ENSURE_EQUAL(secret, Secret(), "Cluster deletion should delete secrets");
+
+	// make reachable and perform the deletion
+	auto kubeConfig = tc.getKubeConfig();
+
 	auto deleteResp=httpDelete(tc.getAPIServerURL()+"/"+currentAPIVersion+"/clusters/"+clusterID+
 				   "?token="+adminKey);
 	ENSURE_EQUAL(deleteResp.status,200,"Cluster deletion should succeed");
