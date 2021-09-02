3cc8057729c65c1ff815585b576d05ea0631f58c
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-09-02 11:52:46 -0600

    working on test-skip-deletion

--- test/TestClusterDeletion.cpp
+++ test/TestClusterDeletion.cpp
@@ -413,13 +413,11 @@ TEST(ForceDeletingUnreachableCluster){
 				   "?token="+adminKey+"&force=true");
 	ENSURE_EQUAL(deleteResp.status,200,"Cluster deletion should succeed");
 
-	// make reachable and perform the full deletion
+	// make reachable
 	startReaper();
 	kubernetes::minikube(minikubeStart);
 	kubernetes::systemctl(kubeletStart);
 	stopReaper();
-
-	ENSURE_EQUAL(deleteResp.status,200,"Cluster deletion should succeed");
 
 	// verify that database records were deleted
 	DatabaseContext db;
@@ -430,6 +428,9 @@ TEST(ForceDeletingUnreachableCluster){
 	auto secret = store.getSecret(secretID);
 	ENSURE_EQUAL(instance, ApplicationInstance(), "Cluster deletion should delete instances");
 	ENSURE_EQUAL(secret, Secret(), "Cluster deletion should delete secrets");
+	
+	// perform full deletion
+	ENSURE_EQUAL(deleteResp.status,200,"Cluster deletion should succeed");
 
 	// Get kubeconfig, save it to file, and use it to check namespaces
 	std::string conf = tc.getKubeConfig();
