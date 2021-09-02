f3fa7feae3f12d3aab94acbd935beeb2f0cd293e
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-09-02 09:15:36 -0600

    working on test-skip-deletion

--- test/TestClusterDeletion.cpp
+++ test/TestClusterDeletion.cpp
@@ -400,8 +400,8 @@ TEST(ForceDeletingUnreachableCluster){
 	}
 
 	// make cluster unreachable
-	std::vector<std::string> stop = {"minikube","status",";","then","minikube","stop",";","else","systemctl","stop","kubelet","fi"};
-	std::vector<std::string> start = {"minikube","status",";","then","minikube","start","--force",";","else","systemctl","start","kubelet","fi"};
+	std::vector<std::string> stop = {"stop",";","systemctl","stop","kubelet"};
+	std::vector<std::string> start = {"start","--force",";","systemctl","start","kubelet"};
 	startReaper();  // disable minikube and/or kubelet
 	kubernetes::minikube(stop);
 	stopReaper();
