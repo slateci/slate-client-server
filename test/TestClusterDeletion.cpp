548184b3d5fc0b5e19ab7be19f6b9c0b9e4a2c9d
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-09-02 17:48:30 -0600

    testing database deletion

--- test/TestClusterDeletion.cpp
+++ test/TestClusterDeletion.cpp
@@ -426,7 +426,7 @@ TEST(ForceDeletingUnreachableCluster){
 	std::cout << "========================================================================" << std::endl;
 	// verify that database records were deleted
 	// DatabaseContext db;
-	auto storePtr=db.makePersistentStore();
+	auto storePtr=tc.makePersistentStore();
 	auto& store=*storePtr;
 
 	auto instance = store.getApplicationInstance(instID);
