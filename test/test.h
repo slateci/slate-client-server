e0a181e75d5193725d32e14770757b3db7f49032
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-09-07 11:38:31 -0600

    fixing database object deletion

--- test/test.h
+++ test/test.h
@@ -153,7 +153,7 @@ public:
 	std::string getPortalUserID() const{ return db.getPortalUserID(); }
 	///Fetch the web-portal user's administrator token
 	std::string getPortalToken() const{ return db.getPortalToken(); }
-	std::unique_ptr<PersistentStore> getDatabaseStore() { return db.makePersistentStore(); }
+	std::unique_ptr<PersistentStore> getDatabaseStore() const{ return db.makePersistentStore(); }
 private:
 	DatabaseContext db;
 	std::string serverPort;
