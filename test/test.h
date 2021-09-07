5ae8608b32cb65af79ad277759d5d84109b42ba1
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-09-07 09:36:32 -0600

    testing database deletion

--- test/test.h
+++ test/test.h
@@ -153,7 +153,7 @@ public:
 	std::string getPortalUserID() const{ return db.getPortalUserID(); }
 	///Fetch the web-portal user's administrator token
 	std::string getPortalToken() const{ return db.getPortalToken(); }
-	DatabaseContext& getDatabaseStore() const{ return db.makePersistentStore(); }
+	std::string getDatabaseStore() const{ return db.makePersistentStore(); }
 private:
 	DatabaseContext db;
 	std::string serverPort;
