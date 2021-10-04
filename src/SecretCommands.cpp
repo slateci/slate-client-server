090ad03a0383258382f6497abe5c40a6e52b2901
root <root@localhost.localdomain>
2021-10-04 19:31:08 -0400

    fixing return statement

--- src/SecretCommands.cpp
+++ src/SecretCommands.cpp
@@ -399,7 +399,7 @@ std::string deleteSecretFromStore(PersistentStore& store, const Secret& secret){
 		log_error("Failed to delete " << secret << " from persistent store");
 		return "Failed to delete secret from database";
 	}
-	return "";
+	return deleteSecretFromStore(store, secret);
 }
 }
 
