8956593145a408e33b2163467a4a0bee63a0e3ac
Adam H Griffith <adam.h.griffith@utah.edu>
2022-04-04 16:58:17 -0600

    Started playing with Rancher.

--- resources/docker/scripts/start-testbench.sh
+++ resources/docker/scripts/start-testbench.sh
@@ -9,4 +9,12 @@ then
   touch /work/.bash_history_docker
 fi
 
+echo "Setting up local Rancher K3s..."
+kubectl config set clusters.default.server https://slate_rancher:6443
+
+#echo "Seeding sample data..."
+#slate cluster create my-cluster --group my-group --org SLATE --no-ingress -y --kubeconfig /etc/rancher/k3s/k3s.yaml
+#echo -e "\e[1m=============================================================\e[0m"
+#echo -e "\e[1mDefault Group: my-group\nDefault Cluster: my-cluster\e[0m"
+
 /bin/bash
