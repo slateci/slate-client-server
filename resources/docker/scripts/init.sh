ffb6148ffef6851bcfe80d418b2223edd8f7b287
Adam H Griffith <adam.h.griffith@utah.edu>
2022-04-06 15:03:35 -0600

    slate-service for localdev no symlink again

--- resources/docker/scripts/init.sh
+++ resources/docker/scripts/init.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 . /slate/resources/docker/scripts/build.sh
 
 echo "Starting slate-service..."
-ln -s /slate/build/slate-service /usr/local/bin/slate-service
-slate-service \
+/slate/build/slate-service \
 --allowAdHocApps True \
 --config "/slate/resources/docker/conf/slate.conf"
