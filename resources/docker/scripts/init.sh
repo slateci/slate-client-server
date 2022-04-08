c968c22057a843df33190517c521fa4978d1b3c8
Adam H Griffith <adam.h.griffith@utah.edu>
2022-04-08 09:40:29 -0600

    Adding slate-service to path for local and release containers.

--- resources/docker/scripts/init.sh
+++ resources/docker/scripts/init.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 . /slate/resources/docker/scripts/build.sh
 
 echo "Starting slate-service..."
-/slate/build/slate-service \
+ln -sf /slate/build/slate-service /usr/bin/slate-service
+slate-service \
 --config "/slate/resources/docker/conf/slate.conf"
