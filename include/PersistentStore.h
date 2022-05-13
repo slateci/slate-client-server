20457e0f28e13b5a1a7c3e221cdee3b67925bcce
Suchandra Thapa <suchandra@gmail.com>
2022-05-13 09:01:06 -1000

    Fix build error

--- include/PersistentStore.h
+++ include/PersistentStore.h
@@ -160,7 +160,7 @@ public:
 	                std::string encryptionKeyFile,
 	                std::string appLoggingServerName,
 	                unsigned int appLoggingServerPort,
-                    std::string const& slateDomain);
+                    std::string slateDomain);
 	
 	///Store a record for a new user
 	///\return Whether the user record was successfully added to the database
