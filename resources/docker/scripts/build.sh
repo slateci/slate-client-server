66f8c4ab0225751de5245e95554e8525f3b5184b
Adam Griffith <adam.h.griffith@utah.edu>
2022-12-15 14:27:37 -0700

    Modified Dockerfile, not build.sh, to determine build dir.

--- resources/docker/scripts/build.sh
+++ resources/docker/scripts/build.sh
@@ -4,6 +4,5 @@
 set -euo pipefail
 
 echo "Building slate-service binary..."
-cd "/slate/build"
 cmake3 .. -DBUILD_CLIENT=False -DBUILD_SERVER=True -DBUILD_SERVER_TESTS=False -DSTATIC_CLIENT=False
 make -k
