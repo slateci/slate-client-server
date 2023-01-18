fa9309778063501fa3aa100f7e5d2cfcf00f65c7
Adam H Griffith <adam.h.griffith@utah.edu>
2023-01-18 10:19:29 -0700

    start_minikube.sh now uses virtualbox by default.

--- clion/start_minikube.sh
+++ clion/start_minikube.sh
@@ -18,7 +18,7 @@ CONTAINER_RUNTIME="containerd"
 KUBERNETES_VERSION="1.24.9"
 MINIKUBE_CNI="calico"
 MINIKUBE_CPUS=2
-MINIKUBE_DRIVER="${1:-podman}" # typical values are docker, podman, virtualbox
+MINIKUBE_DRIVER="${1:-virtualbox}" # typical values are docker, podman, virtualbox
 MINIKUBE_MEMORY=4092 # in Megabytes
 MINIKUBE_NODES=2
 MINIKUBE_PROFILE="slate-$(whoami)"
@@ -26,6 +26,7 @@ MINIKUBE_PROFILE="slate-$(whoami)"
 # Start Minikube:
 if [[ "${MINIKUBE_DRIVER}" == "docker" ]] || [[ "${MINIKUBE_DRIVER}" == "podman" ]]
 then
+  echo "IMPORTANT: This option is still under development -- trying to get kubectl working correctly"
   minikube start \
     --addons="metrics-server" \
     --cni="${MINIKUBE_CNI}" \
